30b7d34223389043767c12db80edae972826e6bf
CarmenViolet <margaretholmes200@gmail.com>
2022-08-29 16:17:40 +0300

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,13 +3,13 @@ let inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', inputChecking);
 
 function inputChecking(event) {
-    if(inputEl.value.length > 6) {
+    if(inputEl.value.length >= 6) {
         if(inputEl.classList.contains('invalid')) {
             inputEl.classList.remove('invalid')
         }
         return inputEl.classList.add('valid')
         
-    } else if(inputEl.value.length < 6) {
+    } else if(inputEl.value.length <= 6) {
         if(inputEl.classList.contains('valid')) {
             inputEl.classList.remove('valid')
         }
